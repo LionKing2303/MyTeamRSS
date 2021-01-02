d94ab439b3931e4614965ec09063cae7b18c85f4
Arie Peretz <ariep20@gmail.com>
2021-01-02 13:42:18 +0200

    Initial Commit

---
+++ MyTeamRSS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTeamRSS
+//
+//  Created by Arie Peretz on 02/01/2021.
+//  Copyright © 2021 Arie Peretz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
